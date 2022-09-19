9b7e6d27f567db0800339442d13f980c469ea1ef
ishadoww <2002004@utrivieramaya.edu.mx>
2022-09-19 10:37:53 -0600

    actualización del proyecto

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import { Productos } from '../components/Productos'
 import { useState } from 'react'
 
-export type product = {
+{/*export type product = {
   category : string  ;
   description: string  ;
   id: number  ;
@@ -19,22 +19,64 @@ const Home= ({productos}: HomeProps) => {
   const [datos, setDatos] = useState(productos)
   return (
     <>
-    <Productos productos={datos}/>
+    <Productos/>
 
     </>
   )
 }
 
+<div>
+  {
+    Productos.map(product =>(
+      <div key={product.id}>
+        <p>{product.category}</p>
+        <p>{product.description}</p>
+        <p>{product.price}</p>
+      </div>
+    ))
+  }
+</div>
+*/}
+
+interface IndexPageProps{
+  data:{
+    id: string
+    description: string
+    price: number
+    category: number
+  }[];
+}
+
+export default function IndexPage({ data }: IndexPageProps){
+  console.log(data);
+  return (
+    <>
+    <h1>Fetching Data from an API</h1>
+    <p>
+      API:  <a href='https://fakestoreapi.com/'>https://fakestoreapi.com/</a>
+    </p>
+    <ul>
+      {
+        data.map(({id, category, description, price}) => (
+          <li key={id}>{category}{description}{price}</li>
+        ))
+        }
+    </ul>
+    </>
+  );
+}
+
+
+
 export const getServerSideProps: GetServerSideProps  = async (context) => {
   
   const response = await fetch("https://fakestoreapi.com/products?limit=5");
-  const productos = await response.json();
-
+  const data = await response.json();
+  //pass data to the page via props
   return {
     props:{
-      productos: productos
+      data
     }
   }
 }
 
-export default Home
